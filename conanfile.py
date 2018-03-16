a811cc83bf1cf26cd7ee771440936408ab7332da
Pedro Vicente <pedrovfer@gmail.com>
2018-03-16 20:12:52 +0000

    Recipe clean removing death code.
    For windows the only compiler allowed is Visual Studio at this point.

--- conanfile.py
+++ conanfile.py
@@ -77,8 +77,6 @@ class VorbisConan(ConanFile):
 
     def build_with_autotools(self):
         env = AutoToolsBuildEnvironment(self)
-        if self.settings.os != "Windows":
-            env.fpic = self.options.fPIC
 
         with tools.environment_append(env.vars):
             with tools.chdir(self.source_subfolder):
@@ -87,20 +85,16 @@ class VorbisConan(ConanFile):
                     new_str = '-install_name \\$soname'
                     tools.replace_in_file("configure", old_str, new_str)
 
-                if self.settings.os == "Windows":
-                    tools.run_in_windows_bash(self, "./configure")
-                    tools.run_in_windows_bash(self, "make")
+                configure_options = " --prefix=%s" % self.package_folder
+                if self.options.shared:
+                    configure_options += " --disable-static --enable-shared"
                 else:
-                    configure_options = " --prefix=%s" % self.package_folder
-                    if self.options.shared:
-                        configure_options += " --disable-static --enable-shared"
-                    else:
-                        configure_options += " --disable-shared --enable-static"
-                    self.run("chmod +x ./configure")
-                    self.run("chmod +x ./install-sh")
-                    self.run("./configure%s" % configure_options)
-                    self.run("make")
-                    self.run("make install")
+                    configure_options += " --disable-shared --enable-static"
+                self.run("chmod +x ./configure")
+                self.run("chmod +x ./install-sh")
+                self.run("./configure%s" % configure_options)
+                self.run("make")
+                self.run("make install")
 
     def build(self):
         if self.settings.compiler == "Visual Studio":
@@ -111,20 +105,17 @@ class VorbisConan(ConanFile):
     def package(self):
         self.copy("FindOGG.cmake")
         self.copy("COPYING", src=self.source_subfolder, keep_path=False)
-        src_include_dir = os.path.join(self.source_subfolder, "include")
-        
-        self.copy("*", dst="include", src=src_include_dir, keep_path=True)
 
         if self.settings.compiler == "Visual Studio":
-            if self.options.shared:
-                self.copy(pattern="*.dll", dst="bin", keep_path=False)
+            # Visual Studio build is not installing installing any artifact.
+            # Packaging needs to be done.
+            src_include_dir = os.path.join(self.source_subfolder, "include")
+            self.copy("*.h", dst="include", src=src_include_dir, keep_path=True)
             self.copy(pattern="*.pdb", dst="bin", keep_path=False)
             self.copy(pattern="*.lib", dst="lib", keep_path=False)
-        else:
+
             if self.options.shared:
-                if self.settings.os == "Windows":
-                    self.copy(pattern="*.dll.a", dst="lib", keep_path=False)
-                    self.copy(pattern="*.dll", dst="bin", keep_path=False)
+                self.copy(pattern="*.dll", dst="bin", keep_path=False)
 
     def package_info(self):
         if self.settings.compiler == "Visual Studio":
